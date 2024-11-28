3ddf2facda9d1318140a3c6c6d0c43840a4d6174
LuckGets <kasdies12@gmail.com>
2024-11-28 17:48:44 +0700

    Setting env & path

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,16 +1,28 @@
+import { lazy } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import GuestLayout from '../layouts/GuestLayout';
 import HomePage from '../pages/guest/HomePage';
-import PageNotFound from '../pages/PageNotFound';
+import { paths } from '@/config/path';
+
+// Import lazy loading page
+const GuestLayout = lazy(() => import('../layouts/GuestLayout'));
+const PageNotFound = lazy(() => import('../pages/PageNotFound'));
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <GuestLayout />,
+    path: paths.app.home.path,
+    lazy: async () => {
+      return { Component: GuestLayout };
+    },
     children: [
       { index: true, element: <HomePage /> },
       { path: '*', element: <PageNotFound /> },
     ],
+  },
+  {
+    path: '*',
+    lazy: async () => {
+      return { Component: PageNotFound };
+    },
   },
 ]);
 
