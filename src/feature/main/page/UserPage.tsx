513a0933fce06203845aa07cea70a0957feb1e78
Apple Srisura <sasita_ssr@kkumail.com>
2024-11-30 01:22:40 +0700

    style:user edit profile

--- src/feature/main/page/UserPage.tsx
+++ src/feature/main/page/UserPage.tsx
@@ -1,19 +1,20 @@
 import UserAvatar from '@/feature/user/components/UserAvatar';
-import UserList from '@/feature/user/UserList';
+import UserList from '@/feature/user/components/UserList';
+import { userInfo } from '@/utils/user/userinfo';
 
 const UserPage = () => {
   return (
-    <div className="flex w-full flex-col text-[#3F3F3F]">
-      <div className="flex flex-col items-center">
+    <div className="flex w-screen flex-col items-center px-14 text-[#3F3F3F] md:px-8 lg:px-16">
+      <div className="flex w-full max-w-4xl flex-col items-center">
         <UserAvatar />
-        <h1 className="mt-4 text-xl font-semibold md:text-3xl">
-          Thawevit kittanmeteeeeee
+        <h1 className="mt-6 text-xl font-semibold md:text-3xl">
+          {userInfo.name}
         </h1>
         <h2 className="text-sm font-semibold text-[#888888] md:text-lg">
-          thaweevit@gmail.com
+          {userInfo.email}
         </h2>
       </div>
-      <div className="mt-4 flex w-full justify-start">
+      <div className="mt-4 flex w-full max-w-4xl justify-start">
         <UserList />
       </div>
     </div>
