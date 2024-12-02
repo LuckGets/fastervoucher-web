e1e75ca7d066b0cdf7e10220febde8979681198f
Apple Srisura <sasita_ssr@kkumail.com>
2024-12-02 16:06:15 +0700

    style:app input

--- src/feature/auth/page/Register.tsx
+++ src/feature/auth/page/Register.tsx
@@ -2,7 +2,7 @@ import RegisterWrapper from '@/feature/auth/components/register/RegisterWrapper'
 
 const Register = () => {
   return (
-    <div className="flex w-full items-center justify-center overflow-x-hidden px-4 py-8">
+    <div className="flex w-full flex-col items-center justify-center overflow-x-hidden px-4 py-8">
       <RegisterWrapper />
     </div>
   );
